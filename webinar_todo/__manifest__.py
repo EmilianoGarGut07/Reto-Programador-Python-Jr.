b875d9a8068267aaddaf6a4e91f11e225fb074c3
EmilianoGarGut07 <emilianogargut07@gmail.com>
2023-08-02 18:05:45 -0600

    prueba

---
+++ webinar_todo/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name":"Webinar Todo",
+    "description":"Aplicacion de prueba",
+    'category': 'Uncategorized',
+    'summary': 'Módulo personalizado para imprimir un texto HTML',
+    'depends': ['base'],
+    'data': [
+        'views/hello_template.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+}
